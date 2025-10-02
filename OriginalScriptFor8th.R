c37d3b4c2a8d4e457aa7a337e707fc6e314384dd
salvadorENR <salvadorestadistic@gmail.com>
2025-10-02 13:27:48 -0600

    hjljkjlk

--- OriginalScriptFor8th.R
+++ OriginalScriptFor8th.R
@@ -2,31 +2,21 @@ install.packages("GDINA", dependencies = T)
 
 library(GDINA)
 
-# set your working directory
-# store the following q matrix csv in your working directory
-
-#setwd("")
-
-# CHANGED: Grade 8 Q-matrix file
+#Loading the updated Q-matrix for 8th grade
 q_mat_8 <- read.csv("Revised Q-matrix2025_09_30_g8.csv", 
                       header=T, stringsAsFactors = F)
 
-# CHANGED: Check if "Item" column exists before removing it
 if("Item" %in% colnames(q_mat_8)) {
   q_mat_8 <- subset(q_mat_8, select=-c(Item))
 }
-
-#q_mat_end_filtered <- q_mat_end[-13, ]
 
 test <- cdmTools::is.Qid(q_mat_8, model = "others")
 
 print(test)
 
-# CHANGED: Grade 8 test data file
 student_test_8<- 
   read.csv("student_test_7_fus.csv", header=T, stringsAsFactors = F)
 
-# CHANGED: Grade 8 column names (G8 instead of G7)
 test_data_8 <- cbind.data.frame(student_test_8$Q1_Correct_or_Wrong_G7,
                                   student_test_8$Q2_Correct_or_Wrong_G7,
                                   student_test_8$Q3_Correct_or_Wrong_G7,
@@ -53,8 +43,6 @@ test_data_8 <- cbind.data.frame(student_test_8$Q1_Correct_or_Wrong_G7,
                                   student_test_8$Q24_Correct_or_Wrong_G7,
                                   student_test_8$Q25_Correct_or_Wrong_G7)
 
-# model comparison: end-line except No.13
-
 est_gdina <- GDINA(dat = test_data_8, Q = q_mat_8, model = "GDINA",
                    mono.constraint = T,
                    control=list(conv.crit=0.000001))
@@ -79,7 +67,6 @@ est_acdm <- GDINA(dat = test_data_8, Q = q_mat_8, model = "ACDM",
                   mono.constraint = T,
                   control=list(conv.crit=0.000001))
 
-#
 
 modelfit(est_gdina)
 modelfit(est_dina)
